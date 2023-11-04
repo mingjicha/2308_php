efef6aa87a53270be1b067fdd193e921c9d2b093
mingjicha <mingi9100@naver.com>
2023-11-05 03:08:00 +0900

    1105 삭제 수정

--- homework/src/07_delete.php
+++ homework/src/07_delete.php
@@ -157,9 +157,9 @@ try {
             <form action="/homework/src/07_delete.php" method="post">
                 <!-- 페이지 버튼 -->
                 <input type="hidden" name="id" value="<?php echo $id; ?>">
-                <div class="del_del">
-                    <button type="submit" class="del_btn">삭제</button>
-                    <a href="/homework/src/06_detail.php/?id=<?php echo $id; ?>&page=<?php echo $page; ?>">취소</a>
+                <div class="delet_btn">
+                    <button type="submit" id="del_de">삭제</button>
+                    <a id="del_li" href="/homework/src/06_detail.php/?id=<?php echo $id; ?>&page=<?php echo $page; ?>">취소</a>
                 </div>
             </form>
         </div>
