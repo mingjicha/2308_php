74a922b9201991d0e7d32de63d3459df5c4b063b
mingjicha <mingi9100@naver.com>
2023-11-13 17:54:55 +0900

    1113 blade템플릿, migrate, seeder

---
+++ larabel-edu/database/migrations/2023_11_13_070434_create_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            // id, no, name, deleted_at
+            $table->id();
+            $table->char('no', 1)->unique; // no char(1) unique not null
+            $table->string('name', 20)->unique(); // name varchar(20) unique not null
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};
